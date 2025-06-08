d87e6a54285f757fecd86a829999bd991766f24d
ToxYc <ivi1000vm@gmail.com>
2025-06-08 17:31:15 +0200

    feat: add description to rsdto

--- src/main/java/org/gycoding/books/infrastructure/api/dto/out/BookRSDTO.java
+++ src/main/java/org/gycoding/books/infrastructure/api/dto/out/BookRSDTO.java
@@ -11,6 +11,7 @@ import java.util.UUID;
 public record BookRSDTO(
         UUID id,
         String title,
+        String description,
         String cover,
         List<AuthorRSDTO> authors,
         SeriesRSDTO series,
