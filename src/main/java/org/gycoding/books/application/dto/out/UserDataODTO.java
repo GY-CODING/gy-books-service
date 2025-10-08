d958ba626ddc650dd5139d5932c0e9490ba0f4e0
ToxYc <ivi1000vm@gmail.com>
2025-10-08 19:36:01 +0200

    hotfix

--- src/main/java/org/gycoding/books/application/dto/out/UserDataODTO.java
+++ src/main/java/org/gycoding/books/application/dto/out/UserDataODTO.java
@@ -11,7 +11,7 @@ public record UserDataODTO(
         String editionId,
         BookStatus status,
         Number rating,
-        String reivew,
+        String review,
         Number progress,
         String startDate,
         String endDate
